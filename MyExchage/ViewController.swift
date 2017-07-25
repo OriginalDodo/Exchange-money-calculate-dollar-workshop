5546438a30539d0dd002ba1db96fee2275c7e83c
Com422Mac03 <com422mac03@Com422Mac03s-iMac.local>
2017-07-25 17:14:00 +0700

    sucess

--- MyExchage/ViewController.swift
+++ MyExchage/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerOutlet: UILabel!
+    @IBOutlet weak var number1Outlet: UITextField!
+    @IBAction func Exchange(_ sender: Any) {
+        
+        let factor = 33.46
+        
+        let douMoney : Double = Double(number1Outlet.text!)!
+        let douAnswer = douMoney * factor
+        
+        let strNumber1 = number1Outlet.text
+        
+        let dounumber1 = Double(strNumber1!)! * 33.3
+        
+        answerOutlet.text = String("change =\(dounumber1)")
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
